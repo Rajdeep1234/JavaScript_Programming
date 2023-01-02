67c247fec0dc595801335cc9e90a229bde54a1e8
Rajdeep <rajdeepgupta28011995@gmail.com>
2023-01-02 16:01:05 +0530

    New Commit

--- Programm/Ex1.js
+++ Programm/Ex1.js
@@ -1,1 +1,17 @@
 console.log("This is Rajdeep");
+let a="Rajdeep"; // var a =9;
+
+// difference between var/const/let
+var b ="Rajdeep";
+{  // making blank block
+    let a="Gupta";
+    console.log(a);
+}
+console.log(a);//Print
+{
+    var b="Gupta"; //var change a keyword permantly
+    console.log(b);
+}
+console.log(b);
+const author="Rajdeep Gupta"; //constant can't be change
+console.log(author);
